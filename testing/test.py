0bd45361512adfbb02e76245c1f6a60d3cf23eb0
sumitvashist <skvd07@gmail.com>
2020-06-29 22:23:58 +0530

    testing example

--- testing/test.py
+++ testing/test.py
@@ -3,8 +3,12 @@ class TestCaseDemo(ut.TestCase):
 	def setUp(self):
 		print("Setup method Execution ... ")
 
-	def test(self):
-		print("test method Execution .. ")
+	def test1(self):
+		print("test method 1 Execution .. ")
+
+
+	def test2(self):
+		print("test method 2 Execution .. ")
 
 	def tearDown(self):
 		print("Tear Down method Execution .. ")
